cb885234a5bc343558a5363a193a9324c6582d62
Yann Bolliger <yann.bolliger@epfl.ch>
2021-04-05 14:51:20 +0200

    Improve usage text.

--- src/index.ts
+++ src/index.ts
@@ -13,19 +13,20 @@ export const cli = (args: string[]) => {
     .describe(
       `Export a block from Notion.so as Markdown or CSV file.
     The block is given by its 'blockId' which is the last part of the URL.
-    Example: https://www.notion.so/Notion-Official-83715d7703ee4b8699b5e659a4712dd8
-    Block Id: 83715d7703ee4b8699b5e659a4712dd8
+    https://www.notion.so/Notion-Official-83715d7703ee4b8699b5e659a4712dd8
+                                          ^^^^^^^      blockId     ^^^^^^^
 
-    For access right's reasons, the 'NOTION_TOKEN' has to be either set on the
-    environment or given to the prompt of the command. The 'NOTION_TOKEN' is the
-    value of the 'token_v2' Cookie used by the official Notion.so API.
+    To download any page, one has to provide the value of the Cookie 'token_v2'
+    of a logged-in user on the official Notion.so API as 'NOTION_TOKEN'
+    environment variable or via the prompt of the command.
+    The user needs to have at least read access to the block/page to download.
 
-    © @yannbolliger, 2021.`
+    © ${pkg.author}, 2021.`
     )
     .option("-t, --type", `File type to be exported: ${FileType}`, "md")
     //  .option("-o, --output", "Output path of the exported file, stdin if empty")
     .example("83715d7703ee4b8699b5e659a4712dd8")
-    .example("3af0a1e347dd40c5ba0a2c91e234b2a5 -t csv")
+    .example("3af0a1e347dd40c5ba0a2c91e234b2a5 -t csv > list.csv")
     //    .example("83715d7703ee4b8699b5e659a4712dd8 -t md -o blog.md")
     .action((blockId, opts) => action(blockId, opts.type))
     .parse(args)
